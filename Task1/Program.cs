5ed5c9a66c2ff2b0eb5f97eda2cbbef381680a4a
Sergey <serega79ser@mail.ru>
2022-05-31 21:28:02 +0600

    Задача 47

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными 
+вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+Console.Clear();
+
+Console.WriteLine("Введи высоту и ширину массива через пробел:");
+int[] lenghts = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+int m = lenghts[0];
+int n = lenghts[1];
+
+double[,] numbers = GetArray(m, n, -9, 9);
+PrintArray(numbers);
+
+double[,] GetArray(int rows, int columns, double min, double max)
+{
+    double[,] result = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (max + 1 - min) + min, 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+
+}
